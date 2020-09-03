4c66da56399ad01d0e7014b350c6468418e91cd4
AdrianoLG <adri@adri.info>
2020-09-03 17:55:39 +0200

    NPM packages update and books invert order

--- api/controllers/library.js
+++ api/controllers/library.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose')
 exports.books_get_all = (req, res, next) => {
 	Book.find({ userId: req.userData.userId })
 		.select('_id title author category pages img read readDate rating comments')
-		.sort({ title: 'desc', category: 'desc' })
+		.sort({ title: 'asc', category: 'asc' })
 		.exec()
 		.then(books => {
 			const response = {
